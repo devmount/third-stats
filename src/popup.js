3d56182a35f2855fd174c04321c2274bd9da5415
Andreas Müller <mail@devmount.de>
2020-10-02 10:10:21 +0200

    ➕ load added translations

--- src/popup.js
+++ src/popup.js
@@ -9,10 +9,18 @@ Vue.config.devtools = false
 import VueI18n from 'vue-i18n'
 Vue.use(VueI18n)
 let messages = {
+	ca: require('../public/_locales/ca/messages.json'),
 	cs: require('../public/_locales/cs/messages.json'),
 	de: require('../public/_locales/de/messages.json'),
 	en: require('../public/_locales/en/messages.json'),
+	es: require('../public/_locales/es/messages.json'),
 	fr: require('../public/_locales/fr/messages.json'),
+	gl: require('../public/_locales/gl/messages.json'),
+	ru: require('../public/_locales/ru/messages.json'),
+	hi: require('../public/_locales/hi/messages.json'),
+	it: require('../public/_locales/it/messages.json'),
+	pl: require('../public/_locales/pl/messages.json'),
+	pt: require('../public/_locales/pt/messages.json'),
 	ru: require('../public/_locales/ru/messages.json'),
 	th: require('../public/_locales/th/messages.json'),
 }
