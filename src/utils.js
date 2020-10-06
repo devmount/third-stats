041401501cbed9a9c51e72e1377ae765e5507c9f
Andreas Müller <mail@devmount.de>
2020-10-06 11:19:09 +0200

    💚 function for extracting email addresses

--- src/utils.js
+++ src/utils.js
@@ -13,4 +13,13 @@ let traverseAccount = (account) => {
 	return arrayOfFolders
 }
 
-export { traverseAccount }
+// function to extract an email address from a given string
+let extractEmailAddress = (s) => {
+	if (s.lastIndexOf("<")>=0 && s.lastIndexOf(">")>=0) {
+		return s.substring(s.lastIndexOf("<") + 1, s.lastIndexOf(">"))
+	} else {
+		return s
+	}
+}
+
+export { traverseAccount, extractEmailAddress }
