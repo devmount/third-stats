cce4d6435710f3fc7a168964a1ede71ec85a87d0
Andreas Müller <mail@devmount.de>
2020-10-08 10:02:45 +0200

    ➕ implemented traditional chinese

--- src/popup.js
+++ src/popup.js
@@ -9,19 +9,20 @@ Vue.config.devtools = false
 import VueI18n from 'vue-i18n'
 Vue.use(VueI18n)
 let messages = {
-	ca: require('../public/_locales/ca/messages.json'), // Catalan
-	cs: require('../public/_locales/cs/messages.json'), // Czech
-	de: require('../public/_locales/de/messages.json'), // German
-	en: require('../public/_locales/en/messages.json'), // English
-	es: require('../public/_locales/es/messages.json'), // Spanish
-	fr: require('../public/_locales/fr/messages.json'), // French
-	gl: require('../public/_locales/gl/messages.json'), // Galician
-	hi: require('../public/_locales/hi/messages.json'), // Hindi
-	it: require('../public/_locales/it/messages.json'), // Italian
-	pl: require('../public/_locales/pl/messages.json'), // Polish
-	pt: require('../public/_locales/pt/messages.json'), // Portuguese
-	ru: require('../public/_locales/ru/messages.json'), // Russian
-	th: require('../public/_locales/th/messages.json'), // Thai
+	'ca':    require('../public/_locales/ca/messages.json'),    // Catalan
+	'cs':    require('../public/_locales/cs/messages.json'),    // Czech
+	'de':    require('../public/_locales/de/messages.json'),    // German
+	'en':    require('../public/_locales/en/messages.json'),    // English
+	'es':    require('../public/_locales/es/messages.json'),    // Spanish
+	'fr':    require('../public/_locales/fr/messages.json'),    // French
+	'gl':    require('../public/_locales/gl/messages.json'),    // Galician
+	'hi':    require('../public/_locales/hi/messages.json'),    // Hindi
+	'it':    require('../public/_locales/it/messages.json'),    // Italian
+	'pl':    require('../public/_locales/pl/messages.json'),    // Polish
+	'pt':    require('../public/_locales/pt/messages.json'),    // Portuguese
+	'ru':    require('../public/_locales/ru/messages.json'),    // Russian
+	'th':    require('../public/_locales/th/messages.json'),    // Thai
+	'zh-tw': require('../public/_locales/zh-tw/messages.json'), // Traditional Chinese
 }
 const i18n = new VueI18n({
 	locale: browser.i18n.getUILanguage(),
