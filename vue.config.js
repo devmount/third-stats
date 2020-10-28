5f36feca72076b4b14149dc31519ee86f4b57ff9
Andreas Müller <mail@devmount.de>
2020-10-28 17:17:16 +0100

    🔨 make webpack bundle filenames static

--- vue.config.js
+++ vue.config.js
@@ -25,7 +25,12 @@ module.exports = {
 		performance: {
 			maxEntrypointSize: 1024000,
 			maxAssetSize: 1024000
-		}
+		},
+		output: {
+			filename: 'js/[name].js',
+			chunkFilename: 'js/[name].bundle.js',
+		},
 	},
+	productionSourceMap: false,
 	publicPath: '',
 }
