0180d34e0a0721140a8cb803a71b31fdc4e0eccf
Andreas Müller <mail@devmount.de>
2020-10-31 05:52:29 +0100

    ➕ implement basic options

---
+++ src/Options.vue
@@ -0,0 +1,91 @@
+<template>
+	<div
+		id='options'
+		:class='{
+			"dark": dark,
+			"light": !dark,
+			"text-normal": true,
+			"background-normal": dark,
+			"background-highlight-contrast": !dark,
+		}'
+	>
+		<div class='container p-1'>
+			<section class='entry'>
+				<label for='dark'>Dark mode</label>
+				<div class='action'>
+					<label class='switch'>
+						<input type='checkbox' id='dark' v-model='dark' />
+						<span class='switch-label' data-on='On' data-off='Off'></span> 
+						<span class='switch-handle'></span> 
+					</label>
+				</div>
+			</section>
+			<section class='entry'>
+				<label for='dark'>Local Identities</label>
+				<div class='action'>
+					<textarea v-model='addresses' placeholder='email1@example.com, email2@example.com'></textarea>
+				</div>
+			</section>
+			<section class='entry mt-5'>
+				<div></div>
+				<div class="action text-right">
+					<button @click='saveSettings'>Save</button>
+				</div>
+			</section>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Options',
+	data () {
+		return {
+			addresses: '',
+			dark: true,
+		}
+	},
+	created () {
+		this.getSettings()
+	},
+	methods: {
+		// get all add-on settings
+		getSettings: async function () {
+			let result = await messenger.storage.local.get('options')
+			this.addresses = result.options.addresses ? result.options.addresses : ''
+			this.dark = result.options.dark ? true : false
+			console.log(result.options);
+		},
+		// save current add-on settings
+		saveSettings: async function () {
+			await messenger.storage.local.set({
+				options: {
+					addresses: this.addresses,
+					dark: this.dark
+				}
+			})
+			console.log('saved');
+		}
+	}
+}
+</script>
+
+<style lang='stylus'>
+@require "assets/global"
+
+// general
+html, body
+	min-height: 300px
+
+// layout
+#options
+	width 100%
+	height 100%
+	min-height: 300px
+
+	.entry
+		display: grid
+		grid-template-columns: 1fr 2fr
+		margin-bottom: 1rem
+
+</style>
