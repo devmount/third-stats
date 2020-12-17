d76151706653eb96ba25a23da6f4b8cab27a24a5
Andreas Müller <mail@devmount.de>
2020-12-17 09:34:53 +0100

    🔨 fix icon and spinner coloring for light mode

--- src/Stats.vue
+++ src/Stats.vue
@@ -25,11 +25,11 @@
 							<svg class='icon icon-bold icon-gray icon-hover-accent' viewBox='0 0 24 24'>
 								<path stroke='none' d='M0 0h24v24H0z' fill='none'/>
 								<path class='icon-part-accent2' d='M9 4.55a8 8 0 0 1 6 14.9m0 -4.45v5h5' />
-								<line class='icon-part-accent2-dark' x1='5.63' y1='7.16' x2='5.63' y2='7.17' />
-								<line class='icon-part-accent2-dark' x1='4.06' y1='11' x2='4.06' y2='11.01' />
-								<line class='icon-part-accent2-dark' x1='4.63' y1='15.1' x2='4.63' y2='15.11' />
-								<line class='icon-part-accent2-dark' x1='7.16' y1='18.37' x2='7.16' y2='18.38' />
-								<line class='icon-part-accent2-dark' x1='11' y1='19.94' x2='11' y2='19.95' />
+								<line class='icon-part-accent2-faded' x1='5.63' y1='7.16' x2='5.63' y2='7.17' />
+								<line class='icon-part-accent2-faded' x1='4.06' y1='11' x2='4.06' y2='11.01' />
+								<line class='icon-part-accent2-faded' x1='4.63' y1='15.1' x2='4.63' y2='15.11' />
+								<line class='icon-part-accent2-faded' x1='7.16' y1='18.37' x2='7.16' y2='18.38' />
+								<line class='icon-part-accent2-faded' x1='11' y1='19.94' x2='11' y2='19.95' />
 							</svg>
 						</div>
 					</div>
