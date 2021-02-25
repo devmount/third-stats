0d4decfff0a2e09b14ce65c9c3e6654900435644
Andreas Müller <mail@devmount.de>
2021-02-25 22:09:36 +0100

    👕 use double quotes in definitions

--- src/definitions.js
+++ src/definitions.js
@@ -1,4 +1,4 @@
-const defaultColors = ['#f9844a', '#f9c74f', '#90be6d', '#43aa8b', '#4d908e', '#577590', '#9c89b8']
+const defaultColors = ["#f9844a", "#f9c74f", "#90be6d", "#43aa8b", "#4d908e", "#577590", "#9c89b8"]
 
 export {
 	defaultColors
