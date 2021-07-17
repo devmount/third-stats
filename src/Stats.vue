b8ecd64663ef92ba9d54d03a192a57577fac8890
Andreas Müller <mail@devmount.de>
2021-07-17 10:47:41 +0200

    ➕ icon and notice for four possibly empty charts

--- src/Stats.vue
+++ src/Stats.vue
@@ -603,25 +603,55 @@
 						<div class="tab-content mt-1">
 							<!-- contacts most emails received from -->
 							<BarChart
-								v-if="tabs.leader.contactsReceived"
+								v-if="tabs.leader.contactsReceived && receivedContactLeadersChartDataExists"
 								:datasets="receivedContactLeadersChartData.datasets"
 								:labels="receivedContactLeadersChartData.labels"
 								:horizontal="true"
 							/>
+							<div v-if="tabs.leader.contactsReceived && !receivedContactLeadersChartDataExists" class="tab-empty text-center mt-5">
+								<svg class="icon icon-large icon-gray icon-thin d-block m-0-auto" viewBox="0 0 24 24">
+									<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+									<path d="M12.983 8.978c3.955 -.182 7.017 -1.446 7.017 -2.978c0 -1.657 -3.582 -3 -8 -3c-1.661 0 -3.204 .19 -4.483 .515m-2.783 1.228c-.471 .382 -.734 .808 -.734 1.257c0 1.22 1.944 2.271 4.734 2.74" />
+									<path d="M4 6v6c0 1.657 3.582 3 8 3c.986 0 1.93 -.067 2.802 -.19m3.187 -.82c1.251 -.53 2.011 -1.228 2.011 -1.99v-6" />
+									<path d="M4 12v6c0 1.657 3.582 3 8 3c3.217 0 5.991 -.712 7.261 -1.74m.739 -3.26v-4" />
+									<line x1="3" y1="3" x2="21" y2="21" />
+								</svg>
+								<div class="text-gray mt-1" v-html="$t('stats.charts.contactsReceived.empty')"></div>
+							</div>
 							<!-- contacts most emails sent to -->
 							<BarChart
-								v-if="tabs.leader.contactsSent"
+								v-if="tabs.leader.contactsSent && sentContactLeadersChartDataExists"
 								:datasets="sentContactLeadersChartData.datasets"
 								:labels="sentContactLeadersChartData.labels"
 								:horizontal="true"
 							/>
+							<div v-if="tabs.leader.contactsSent && !sentContactLeadersChartDataExists" class="tab-empty text-center mt-5">
+								<svg class="icon icon-large icon-gray icon-thin d-block m-0-auto" viewBox="0 0 24 24">
+									<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+									<path d="M12.983 8.978c3.955 -.182 7.017 -1.446 7.017 -2.978c0 -1.657 -3.582 -3 -8 -3c-1.661 0 -3.204 .19 -4.483 .515m-2.783 1.228c-.471 .382 -.734 .808 -.734 1.257c0 1.22 1.944 2.271 4.734 2.74" />
+									<path d="M4 6v6c0 1.657 3.582 3 8 3c.986 0 1.93 -.067 2.802 -.19m3.187 -.82c1.251 -.53 2.011 -1.228 2.011 -1.99v-6" />
+									<path d="M4 12v6c0 1.657 3.582 3 8 3c3.217 0 5.991 -.712 7.261 -1.74m.739 -3.26v-4" />
+									<line x1="3" y1="3" x2="21" y2="21" />
+								</svg>
+								<div class="text-gray mt-1" v-html="$t('stats.charts.contactsSent.empty')"></div>
+							</div>
 							<!-- contacts flagged as junk -->
 							<BarChart
-								v-if="tabs.leader.contactsJunk"
+								v-if="tabs.leader.contactsJunk && junkContactLeadersChartDataExists"
 								:datasets="junkContactLeadersChartData.datasets"
 								:labels="junkContactLeadersChartData.labels"
 								:horizontal="true"
 							/>
+							<div v-if="tabs.leader.contactsJunk && !junkContactLeadersChartDataExists" class="tab-empty text-center mt-5">
+								<svg class="icon icon-large icon-gray icon-thin d-block m-0-auto" viewBox="0 0 24 24">
+									<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+									<path d="M12.983 8.978c3.955 -.182 7.017 -1.446 7.017 -2.978c0 -1.657 -3.582 -3 -8 -3c-1.661 0 -3.204 .19 -4.483 .515m-2.783 1.228c-.471 .382 -.734 .808 -.734 1.257c0 1.22 1.944 2.271 4.734 2.74" />
+									<path d="M4 6v6c0 1.657 3.582 3 8 3c.986 0 1.93 -.067 2.802 -.19m3.187 -.82c1.251 -.53 2.011 -1.228 2.011 -1.99v-6" />
+									<path d="M4 12v6c0 1.657 3.582 3 8 3c3.217 0 5.991 -.712 7.261 -1.74m.739 -3.26v-4" />
+									<line x1="3" y1="3" x2="21" y2="21" />
+								</svg>
+								<div class="text-gray mt-1" v-html="$t('stats.charts.contactsJunk.empty')"></div>
+							</div>
 						</div>
 					</div>
 					<!-- section: folders -->
@@ -673,11 +703,21 @@
 						<div class="tab-content mt-1">
 							<!-- tags count -->
 							<BarChart
-								v-if="tabs.tags.tagsCount"
+								v-if="tabs.tags.tagsCount && tagsChartDataExists"
 								:datasets="tagsChartData.datasets"
 								:labels="tagsChartData.labels"
 								:horizontal="true"
 							/>
+							<div v-if="tabs.tags.tagsCount && !tagsChartDataExists" class="tab-empty text-center mt-5">
+								<svg class="icon icon-large icon-gray icon-thin d-block m-0-auto" viewBox="0 0 24 24">
+									<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+									<path d="M12.983 8.978c3.955 -.182 7.017 -1.446 7.017 -2.978c0 -1.657 -3.582 -3 -8 -3c-1.661 0 -3.204 .19 -4.483 .515m-2.783 1.228c-.471 .382 -.734 .808 -.734 1.257c0 1.22 1.944 2.271 4.734 2.74" />
+									<path d="M4 6v6c0 1.657 3.582 3 8 3c.986 0 1.93 -.067 2.802 -.19m3.187 -.82c1.251 -.53 2.011 -1.228 2.011 -1.99v-6" />
+									<path d="M4 12v6c0 1.657 3.582 3 8 3c3.217 0 5.991 -.712 7.261 -1.74m.739 -3.26v-4" />
+									<line x1="3" y1="3" x2="21" y2="21" />
+								</svg>
+								<div class="text-gray mt-1" v-html="$t('stats.charts.tagsCount.empty')"></div>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -2138,6 +2178,10 @@ export default {
 				labels: Object.keys(s)
 			}
 		},
+		// true if there are any receiver contacts to display
+		sentContactLeadersChartDataExists () {
+			return this.sentContactLeadersChartData.datasets[0].data?.length > 0;
+		},
 		// prepare data for received emails leaderboard horizontal bar chart
 		receivedContactLeadersChartData () {
 			const r = this.display.contacts.received
@@ -2151,6 +2195,10 @@ export default {
 				],
 				labels: Object.keys(r)
 			}
+		},
+		// true if there are any sender contacts to display
+		receivedContactLeadersChartDataExists () {
+			return this.receivedContactLeadersChartData.datasets[0].data?.length > 0;
 		},
 		// prepare data for junk emails leaderboard horizontal bar chart
 		junkContactLeadersChartData () {
@@ -2166,6 +2214,10 @@ export default {
 				],
 				labels: Object.keys(r)
 			}
+		},
+		// true if there are any junk contacts to display
+		junkContactLeadersChartDataExists () {
+			return this.junkContactLeadersChartData.datasets[0].data?.length > 0;
 		},
 		// prepare data for emails per folder doughnut charts
 		foldersChartData () {
@@ -2213,6 +2265,10 @@ export default {
 				],
 				labels: labels
 			}
+		},
+		// true if there is any tags data to display
+		tagsChartDataExists () {
+			return this.tagsChartData.datasets[0].data?.length > 0;
 		},
 		// returns true, if at least one filter isn't empty
 		filtered () {
@@ -2421,7 +2477,7 @@ body
 				column-gap: 2rem
 				row-gap: 1rem
 				transition: grid-template-columns .2s
-				& > *, .tab-content:not(.chart-group) > *
+				& > *, .tab-content:not(.chart-group) > *:not(.tab-empty)
 					min-height: 380px
 				.chart
 					min-width: 0
