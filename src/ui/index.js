23097bf42b6d5cec61f35e21b340bde4da65a156
Andreas Müller <mail@devmount.de>
2026-08-23 23:52:21 +0200

    🔨 Use @ alias for imports

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,13 +1,13 @@
-import TsIcon from './TsIcon.vue';
-import TsButton from './TsButton.vue';
-import TsCheckbox from './TsCheckbox.vue';
-import TsSwitch from './TsSwitch.vue';
-import TsInputGroup from './TsInputGroup.vue';
-import TsTag from './TsTag.vue';
-import TsLoader from './TsLoader.vue';
-import TsSelect from './TsSelect.vue';
-import TsCharInput from './TsCharInput.vue';
-import TsColorInput from './TsColorInput.vue';
+import TsIcon from '@/ui/TsIcon.vue';
+import TsButton from '@/ui/TsButton.vue';
+import TsCheckbox from '@/ui/TsCheckbox.vue';
+import TsSwitch from '@/ui/TsSwitch.vue';
+import TsInputGroup from '@/ui/TsInputGroup.vue';
+import TsTag from '@/ui/TsTag.vue';
+import TsLoader from '@/ui/TsLoader.vue';
+import TsSelect from '@/ui/TsSelect.vue';
+import TsCharInput from '@/ui/TsCharInput.vue';
+import TsColorInput from '@/ui/TsColorInput.vue';
 import vTooltip from '@/directives/tooltip.js';
 
 export default {
